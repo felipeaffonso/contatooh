7d537f1e3533df66ec1b309e1a782e0791e9b227
Felipe Zanardo Affonso <felipe.z.affonso@gmail.com>
2016-01-27 10:55:15 -0200

    Adicionando testes com protractor

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -2,12 +2,15 @@
 
 describe("ContatoController", function() {
 
-	var $scope;
+	var $scope, $httpBackend;
 
 	beforeEach(function() {
 		module('contatooh');
-		inject(function($injector) {
+		inject(function($injector, _$httpBackend_) {
 			$scope = $injector.get('$rootScope').$new();
+			$httpBackend = _$httpBackend_;
+			$httpBackend.when('GET', '/contatos/1').respond({_id: '1'});
+			$httpBackend.when('GET', '/contatos').respond([{}]);
 		});
 	});
 
@@ -15,6 +18,17 @@ describe("ContatoController", function() {
 		inject(function($controller) {
 			$controller('ContatoController', {'$scope': $scope});
 			expect($scope.contato._id).toBeUndefined();
+		})
+	);
+
+	it("Deve preencher Contato quando parâmetro de rota for passado",
+		inject(function($controller) {
+			$controller('ContatoController', {
+				'$routeParams' : {contatoId: 1},
+				'$scope': $scope
+			});
+			$httpBackend.flush();
+			expect($scope.contato._id).toBeDefined();
 		})
 	);
 });
