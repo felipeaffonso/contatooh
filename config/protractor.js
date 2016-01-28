349b16a231617a1053100a41df601eba2f29dfed
Felipe Zanardo Affonso <felipe.z.affonso@gmail.com>
2016-01-28 08:02:39 -0200

    Adicionando suporte as variaveis de ambiente

--- config/protractor.js
+++ config/protractor.js
@@ -1,12 +1,14 @@
 // config/protractor.js
 
+var config = require('./config')();
+
 exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 	onPrepare: function() {
 		browser.driver.get('http://localhost:3000').then(function() {
 			browser.driver.findElement(by.id('entrar')).click();
-			browser.driver.findElement(by.id('login_field')).sendKeys('felipe.z.affonso@gmail.com');
-			browser.driver.findElement(by.id('password')).sendKeys('9qR8DpIz2qZy');
+			browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
+			browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
 			browser.driver.findElement(by.name('commit')).click();
 		});
 	}
